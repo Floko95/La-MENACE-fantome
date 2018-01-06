b3a5caf85e581accbe8408e9eb9ef3a93607665a
Floko95 <flo.kot@free.fr>
2018-01-06 20:29:08 +0100

    CreerMasque - 1 execution error left

--- Code/menace.c
+++ Code/menace.c
@@ -3,19 +3,16 @@
 #include <stdint.h>
 #include "menace.h"
 
-int main(int argc, char const *argv[]) {
 
-  return 0;
-}
 
 int set_case(int terrain, int cases, int value) // cases diot etre le numéro de la case; value doit etre CROIX ROND ou VIDE
 {
-    return terrain|(value<<((case-1)*2));
+    return terrain|(value<<((cases-1)*2));
 }
 
 int get_case(int terrain, int cases)// cases diot etre une des constant le numéro de la case;
 {
-  return (terrain&(3<<((case-1)*2)))/(cases/3);
+  return (terrain&(3<<((cases-1)*2)))/(cases/3);
 }
 
 int nb_case_libre(int terrain)
@@ -29,9 +26,9 @@ int nb_case_libre(int terrain)
   return cpt;
 }
 
-int CreerMasque(int case, int typeMasque)		//case de 1 à 9 et non de 0 à 8
+int CreerMasque(int Case, int typeMasque)		//case de 1 à 9 et non de 0 à 8
 {
-	assert(case==0);
+	assert(Case!=0);
 	uint64_t masque=0;
 	uint32_t limite;
 	uint32_t i;
@@ -46,12 +43,20 @@ int CreerMasque(int case, int typeMasque)		//case de 1 à 9 et non de 0 à 8
 		limite=64;						//les billes totales sur 64 bits
 		stockage_info=7;				//les billes d'une case sur 7 bits
 	}
-	
+
 	for(i=0;i<limite;i++)						//création bit par bit du masque
 	{
-		if(i< case * stockage_info & i>= (case-1) * stockage_info)		//si le bit parcouru est  un bit de la case souhaitée	
+		if(i< Case * stockage_info & i>= (Case-1) * stockage_info)		//si le bit parcouru est  un bit de la case souhaitée
 			masque+= pow(2,i);					//on met le bit à 1 ( donc on ajoute 2^i)
 												//sinon il sera à 0 et on passe au bit suivant
 	}
 	return masque;
+}
+int main(int argc, char const *argv[]) {
+
+	printf("%d\n",CreerMasque(1,MASQUE_BILLE));
+	printf("%d\n",CreerMasque(9,MASQUE_BILLE));
+	printf("%d\n",CreerMasque(3,MASQUE_CASE));
+	printf("%d\n",CreerMasque(9,MASQUE_CASE));
+  return 0;
 }
