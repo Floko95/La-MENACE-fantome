b8e1e319c40d2a83be1daa6cecd867580996846f
LoicAulard <35135872+LoicAulard@users.noreply.github.com>
2018-01-09 22:23:48 +0100

    Update menace.h

--- Code/menace.h
+++ Code/menace.h
@@ -4,25 +4,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+
 #define SIZE 9
-
-#define LINE1 63
-#define LINE2 4032
-#define LINE3 258048
-
-#define COLUMN1 12483
-#define COLUMN2 49932
-#define COLUMN3 199728
-
-#define CASE1 3
-#define CASE2 12
-#define CASE3 48
-#define CASE4 192
-#define CASE5 768
-#define CASE6 3072
-#define CASE7 12288
-#define CASE8 49152
-#define CASE9 196608
 
 #define CROIX 2
 #define ROND  1
@@ -30,12 +13,12 @@
 
 #define MASQUE_CASE 0
 #define MASQUE_BILLE 1
+
 typedef struct _boite
 {
   int terrain;
-  int *possibilite;
+  uint64_t bille
   struct _boite **suivants;
-  struct _boite *pere;
 } boite;
 
 
