bf8f389997dc7f3e6e0413adfb1ae3875283eb5f
LoicAulard <35135872+LoicAulard@users.noreply.github.com>
2018-01-14 15:13:44 +0100

    Update menace.h

--- Code/menace.h
+++ Code/menace.h
@@ -26,6 +26,9 @@ typedef struct _boite
 
 int set_case(int terrain, int cases, int value);
 int get_case(int terrain, int cases);
+uint64_t get_bille(uint64_t billes, int Case);
+uint64_t set_bille(uint64_t billes, int Case, int value);
+uint64_t sommeBilles(uint64_t billes);
 int nb_case_libre(int terrain);
 int symetrie_v(int32_t t1, int32_t t2);
 int symetrie_h(int32_t t1, int32_t t2);
@@ -38,7 +41,16 @@ void creer_graphe(boite* b, int32_t* figure, boite** addresse);
 int32_t verify_tab(int32_t terrain, int32_t* tab);
 boite* creer_noeud(int32_t terrain);
 void afficheTerrain(uint32_t terrain);
-
+void charger_graphe(boite* b, boite** adresse, FILE* file);
+void save_graphe(boite* b, boite** adresse, FILE* file);
+void free_graphe(boite* b, boite** adresse);
+int checkfree(boite* b, boite** adresse);
+int ProchainCoup(uint64_t billes);
+ int partieFinie(uint32_t terrain);
+//---Interface utilisateur
+int promptCoup();
+int32_t promptGamemode();
+boite* choix_graphe()
 
 
 #endif
